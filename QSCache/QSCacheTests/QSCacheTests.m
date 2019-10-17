e84552f8e13f084a16fe2b59d03e26bfbbe9c833
wuqiushan <wuqiushan3@163.com>
2019-10-17 10:04:02 +0800

    创建工程

---
+++ QSCache/QSCacheTests/QSCacheTests.m
@@ -0,0 +1,37 @@
+//
+//  QSCacheTests.m
+//  QSCacheTests
+//
+//  Created by wuqiushan on 2019/10/17.
+//  Copyright © 2019 wuqiushan3@163.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QSCacheTests : XCTestCase
+
+@end
+
+@implementation QSCacheTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
